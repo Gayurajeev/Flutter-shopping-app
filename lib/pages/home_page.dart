b76868d627adf4a5cda88b446edce19d8664bddc
GAYATHRY RAJEEV <gayathry.rs22@gmail.com>
2025-01-21 01:46:43 +0530

    Another page created

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../components/bottom_nav_bar.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar:MyBottomNavBar(
+        onTabChange: (index)=> navigateBottomBar(index),
+      ) ,
+    );
+  }
+}
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -9,30 +11,73 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Column(
-        children: [
-          // logo
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Image.asset('lib/images/logo.png',
-              height: 240,
-            ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/logo.png',
+                  height: 240,
+                ),
+              ),
+              const SizedBox(height: 48),
+              // title
+              const Text(
+                'Fashionably More',
+                style:  TextStyle(
+                  fontWeight:FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+                      const SizedBox(height: 24),
+
+              // sub title
+              const Text(
+                'Brand new heels made with premium quality',
+                style:  TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+                textAlign:TextAlign.center ,
+              ),
+              const SizedBox(height: 48),
+
+              // start now button
+              GestureDetector(
+                onTap:() => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                  builder:(context)=>HomePage(),
+                ),
+                ),
+                child: Container(
+                  decoration:BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12)
+                  ) ,
+                  padding:const EdgeInsets.all(25) ,
+                  child: const Center(
+                    child: const Text(
+                        'Shop Now',
+                      style: TextStyle(
+                          color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+
+            ],
+                  ),
           ),
-          const SizedBox(height: 48),
-          // title
-          Text(
-            'Just Do It',
-            style:  TextStyle(
-              fontWeight:FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          // sub title
-          // start now button
-        ],
-      ),
-    ),
+        ),
     );
+
 
   }
 }
