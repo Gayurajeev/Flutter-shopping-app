be12e7be4b54de6eba7610a534460b0512431556
GAYATHRY RAJEEV <gayathry.rs22@gmail.com>
2025-01-20 19:46:04 +0530

    Added subtitle,changed fontsize

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,7 +8,30 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-
+      body: Center(
+        child: Column(
+        children: [
+          // logo
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Image.asset('lib/images/logo.png',
+              height: 240,
+            ),
+          ),
+          const SizedBox(height: 48),
+          // title
+          Text(
+            'Just Do It',
+            style:  TextStyle(
+              fontWeight:FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          // sub title
+          // start now button
+        ],
+      ),
+    ),
     );
 
   }
