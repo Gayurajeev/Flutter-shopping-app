0da6d18a08f0204417804ade978af841affb3fd7
GAYATHRY RAJEEV <gayathry.rs22@gmail.com>
2025-01-26 00:48:33 +0530

    Modifications made in cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../components/cart_item.dart';
+import '../models/cart.dart';
+import '../models/shoe.dart';
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+const CartPage({super.key});
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -9,6 +13,26 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center (child: Text('cart'),);
+    return Consumer<Cart>(
+        builder: (context,value,child)=>Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              Text('My Cart',
+            style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24),
+              ),
+              const SizedBox(height: 10),
+              Expanded(child: ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder: (context,index) {
+                Shoe individualShoe = value.getUserCart()[index];
+                return CartItem(shoe: individualShoe,);
+              },
+          ),
+        )
+        ],
+    ),
+    ),
+    );
   }
 }
